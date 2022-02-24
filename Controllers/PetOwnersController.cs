5394495f37a4d31adc750ed9ed4c871ecf0f29d8
Christopher Razidlo <cd.razidlo@gmail.com>
2022-02-24 10:10:08 -0600

    post petowner complete

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,15 +12,26 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
+        public IEnumerable<PetOwner> GetPets()
+        {
             return _context.PetOwners;
+        }
+
+        [HttpPost]
+        public ActionResult<PetOwner> Post(PetOwner petOwner)
+        {
+            // Add petOwner to the database
+            _context.Add(petOwner);
+            _context.SaveChanges();
+            return petOwner;
         }
     }
 }
